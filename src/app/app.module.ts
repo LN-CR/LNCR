b3ede94c83d2467551fce46aa62bdc0eea99148e
Chad Dayton <cdayton96@gmail.com>
2021-03-01 17:49:21 -0500

    feat: added skeletons for core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EmployersComponent } from './employers/employers.component';
+import { FreelancersComponent } from './freelancers/freelancers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    ProfileComponent,
+    EmployersComponent,
+    FreelancersComponent
   ],
   imports: [
     BrowserModule,
