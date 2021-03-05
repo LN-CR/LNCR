e2a9fdca798300bb786c6378cf7fea5409a507f3
Iskak Mantyubetov <ayzik55@gmail.com>
2021-03-05 17:38:05 -0500

    implemented validations for signup inputs

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,13 +10,19 @@ import { FormBuilder, Validators } from '@angular/forms';
 
 export class SignupComponent implements OnInit {
 
-  firstName = '';
-  lastName = '';
   email = '';
+  password = '';
+  confirmPassword = '';
+  oneNumber = false;
+  oneLowerCaseLatter = false;
+  oneUpperCaseLatter = false;
+  passwordLength = false;
+  latters = false;
+
   authForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', Validators.required]
+    email: ['', Validators.email],
+    password: ['', Validators.required],
+    confrimPassword: ['', Validators.required]
   });
 
   constructor(private fb: FormBuilder ) {}
@@ -25,7 +31,37 @@ export class SignupComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.authForm);
-    this.firstName = this.authForm.value.firstName;
+    console.log(this.authForm.value.password);
+    console.log(typeof parseInt(this.authForm.value.password[this.authForm.value.password.length - 1], 16));
+
+    this.email = this.authForm.value.email;
+  }
+  onChange(): void {
+
+    const length = this.authForm.value.password.length;
+    const lastChar = this.authForm.value.password
+    [this.authForm.value.password.length - 1];
+    const lowerCase = 'qwertyuioplkjhgfdsazxcvbnm';
+    const upperCase = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+
+    if ('1234567890'.includes(lastChar) && !this.oneNumber){
+      this.oneNumber = true;
+    }
+
+    if (!this.oneUpperCaseLatter && upperCase.includes(lastChar)){
+      this.oneUpperCaseLatter = true;
+    }
+
+    if ( !this.oneLowerCaseLatter && lowerCase.includes(lastChar)){
+      this.oneLowerCaseLatter = true;
+    }
+
+    if (!this.latters && this.oneLowerCaseLatter && this.oneUpperCaseLatter){
+      this.latters = true;
+    }
+
+    if (!this.passwordLength && length > 7){
+      this.passwordLength = true;
+    }
   }
 }
