eacd08548e1e42e811ee187246adb31f5e67b6ea
Linh Vu <thuylinh2490@gmail.com>
2021-03-04 23:19:00 -0500

    feat(login): add form and style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  email = '';
+  authForm = this.fb.group({
+    email: ['', Validators.required],
+   password: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(): void {
+    console.log(this.authForm);
+    this.email = this.authForm.value.email;
   }
 
 }
